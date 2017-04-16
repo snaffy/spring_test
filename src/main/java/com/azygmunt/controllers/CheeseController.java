57267a1296784842923f377835db62132540aa91
Artur <art.zgmt@gmail.com>
2017-04-16 18:49:27 +0200

    Initial Commit

---
+++ src/main/java/com/azygmunt/controllers/CheeseController.java
@@ -0,0 +1,39 @@
+package com.azygmunt.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+
+/**
+ * Created by azygm on 15.04.2017.
+ */
+@Controller
+@RequestMapping("cheese")
+public class CheeseController {
+    static ArrayList<String> cheeses = new ArrayList<>();
+
+    @RequestMapping(value = "")
+    public String index(Model model){
+
+        model.addAttribute("cheeses", cheeses);
+        model.addAttribute("title","My Cheese");
+        return "cheese/index";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.GET)
+    public String displayAddCheeseForm(Model model){
+        model.addAttribute("title","Add Cheese");
+        return "cheese/add";
+    }
+
+    @RequestMapping(value = "add", method = RequestMethod.POST)
+    public String processAddCheeseForm(@RequestParam String cheeseName){
+        cheeses.add(cheeseName);
+        return "redirect:";
+    }
+}
